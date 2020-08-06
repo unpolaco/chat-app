ffa2dccc7c372a9278b733ad0ed30697b85e8a64
Michal Borowski <unpolacoo@gmail.com>
2020-08-06 18:10:03 +0200

    Add router paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Route path='/login' component={Login}></Route>
+        <Route path='/signup' component={SignUp}></Route>
+        <Route path='/dashboard' component={Dashboard}></Route>
+      </Router>
     </div>
   );
 }
