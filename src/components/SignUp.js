95af66dc6dc43e602c96801ebd4e461037614f2e
Michal Borowski <unpolacoo@gmail.com>
2020-08-07 10:19:58 +0200

    Configure firebase and sign up user to server

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,77 +1,103 @@
 import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+const firebase = require('firebase');
 
 export default function SignUp() {
+	const [nickName, setNickName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [passwordConfirm, setPasswordConfirm] = useState('');
+	const [signUpError, setSignUpError] = useState('');
+	let history = useHistory();
 
-  const [nickName, setNickName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
 
-  function handleSubmit(e) {
-      e.preventDefault();
-      if(password !== passwordConfirm) {
-        alert("Wrong password confirmation!")
-        setPassword('')
-        setPasswordConfirm('')
-      }
-  }
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (password !== passwordConfirm) {
+			alert('Wrong password confirmation!');
+			setPassword('');
+			setPasswordConfirm('');
+		}
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(email, password)
+			.then((authRes) => {
+				const userObj = {
+					email: authRes.user.email,
+				};
+				firebase
+					.firestore()
+					.collection('users')
+					.doc(email)
+					.set(userObj)
+					.then(() => {
+						history.push('./dashboard');
+					}, dbError => {
+            console.log(dbError);
+            setSignUpError('Failed to add user');
+          });
+			}, authErr => {
+        console.log(authErr);
+        setSignUpError('Failed to add user')
+      });
+	}
 
-  function handleTypingNick(e) {
-    setNickName(e.target.value)
-  }
-  function handleTypingEmail(e) {
-    setEmail(e.target.value)
-  }
-  function handleTypingPassword(e) {
-    setPassword(e.target.value)
-  }
-  function handleTypingPasswordConfirm(e) {
-    setPasswordConfirm(e.target.value)
-  }
+	function handleTypingNick(e) {
+		setNickName(e.target.value);
+	}
+	function handleTypingEmail(e) {
+		setEmail(e.target.value);
+	}
+	function handleTypingPassword(e) {
+		setPassword(e.target.value);
+	}
+	function handleTypingPasswordConfirm(e) {
+		setPasswordConfirm(e.target.value);
+	}
 
 	return (
 		<div>
 			<h3>Sign Up!</h3>
 			<form>
 				<label htmlFor='nickName'>Enter your nick name</label>
-				<input 
-          autoFocus 
-          autoComplete         
-          onChange={(e) => handleTypingNick(e)} 
-          value={nickName} 
-          id='nickName' 
-          type='text' 
-        />
+				<input
+					autoFocus
+					autoComplete
+					onChange={(e) => handleTypingNick(e)}
+					value={nickName}
+					id='nickName'
+					type='text'
+				/>
 			</form>
 			<form>
 				<label htmlFor='email'>Enter your e-mail</label>
-				<input 
-          autoComplete
-          onChange={(e) => handleTypingEmail(e)} 
-          value={email} 
-          id='email' 
-          type='text' 
-        />
+				<input
+					autoComplete
+					onChange={(e) => handleTypingEmail(e)}
+					value={email}
+					id='email'
+					type='text'
+				/>
 			</form>
 			<form>
 				<label htmlFor='password'>Create your password</label>
 				<input
-          autoComplete 
-          onChange={(e) => handleTypingPassword(e)} 
-          value={password} 
-          id='password' 
-          type='password' 
-        />
+					autoComplete
+					onChange={(e) => handleTypingPassword(e)}
+					value={password}
+					id='password'
+					type='password'
+				/>
 			</form>
 			<form>
 				<label htmlFor='passwordConfirm'>Confirm your password</label>
 				<input
-          autoComplete 
-          onChange={(e) => handleTypingPasswordConfirm(e)} 
-          value={passwordConfirm} 
-          id='passwordConfirm' 
-          type='password' 
-        />
+					autoComplete
+					onChange={(e) => handleTypingPasswordConfirm(e)}
+					value={passwordConfirm}
+					id='passwordConfirm'
+					type='password'
+				/>
 			</form>
 			<button onClick={handleSubmit}>Submit</button>
 		</div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require("firebase")
+require("firebase/firestore")
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAM7OSmUzTEdz9wvO6gJaWqX4rPilXeQAM",
+    authDomain: "chatapp-4ff48.firebaseapp.com",
+    databaseURL: "https://chatapp-4ff48.firebaseio.com",
+    projectId: "chatapp-4ff48",
+    storageBucket: "chatapp-4ff48.appspot.com",
+    messagingSenderId: "755440019702",
+    appId: "1:755440019702:web:3a93b71f595c79415564d7"
+
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
