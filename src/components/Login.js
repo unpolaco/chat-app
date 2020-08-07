df2dd5370157bbd189e102b40f8df02517b51b54
Michal Borowski <unpolacoo@gmail.com>
2020-08-07 19:52:06 +0200

    Create Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+const firebase = require('firebase');
 
 export default function Login() {
-  return (
-    <div>
-      <p>Login</p>
-    </div>
-  )
-};
+  const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [loginError, setLoginError] = useState(null)
+	let history = useHistory();
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		firebase
+		.auth()
+		.signInWithEmailAndPassword(email, password)
+			.then(() => {
+				history.push('./dashboard');
+				}, err => {
+					setLoginError('Server error');
+					console.log(err);
+					});
+	}
+
+	function handleTypingEmail(e) {
+		setEmail(e.target.value);
+	}
+	function handleTypingPassword(e) {
+		setPassword(e.target.value);
+	}
+
+	return (
+		<div>
+			<h3>Log in to the Chat</h3>
+			<form>
+				<label htmlFor='email'>Enter your e-mail</label>
+				<input
+          autoFocus
+					autoComplete
+					onChange={(e) => handleTypingEmail(e)}
+					value={email}
+					id='email'
+					type='text'
+				/>
+			</form>
+			<form>
+				<label htmlFor='password'>Enter your password</label>
+				<input
+					autoComplete
+					onChange={(e) => handleTypingPassword(e)}
+					value={password}
+					id='password'
+					type='password'
+				/>
+			</form>
+			<button onClick={handleSubmit}>Submit</button>
+		</div>
+	);
+}
