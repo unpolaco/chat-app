7908e768c08080fac200ea1d8301751748fb1f66
Michal Borowski <unpolacoo@gmail.com>
2020-08-11 12:45:50 +0200

    check if there are already any chats in ChatList

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -12,18 +12,21 @@ export default function ChatList(props) {
 			<p>Hello from Chatlist!</p>
 			<button onClick={newChat}>New chat</button>
 			<ul>
-				{props.chats.map((chat, index) => {
-					return (
-            <li key={index} onClick={() => {selectChat(index)}}>
-              <p>
-                {chat.users.filter( user => user !== props.userEmail)[0]}
-              </p>
-              <p>
-                {chat.messages[chat.messages.length - 1].message.substring(0,30)}
-              </p>
-            </li>
-          )
-				})}
+        {props.chats.length > 0 ? 
+          props.chats.map((chat, index) => {
+            return (
+              <li key={index} onClick={() => {selectChat(index)}}>
+                <p>
+                  {chat.users.filter( user => user !== props.userEmail)[0]}
+                </p>
+                <p>
+                  {chat.messages[chat.messages.length - 1].message.substring(0,30)}
+                </p>
+              </li>
+            )
+          }) :
+          null
+        }
 			</ul>
 		</div>
 	);
