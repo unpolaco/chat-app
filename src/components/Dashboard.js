7225d2bb63e1c00d7a5aba34894d74978a05c7c8
Michal Borowski <unpolacoo@gmail.com>
2020-08-11 10:37:42 +0200

    Add states to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ChatList from './ChatList'
+import { useHistory } from "react-router-dom";
+const firebase = require('firebase');
+
 
 export default function Dashboard() {
+
+const [selectedChat, setSelectedChat] = useState(null)
+const [chats, setChats] = useState([])
+const [userEmail, setUserEmail] = useState(null)
+const [userName, setUserName] = useState(null)
+const [newChatVisible, setNewChatVisible] = useState(false)
+let history = useHistory();
+
+
+const selectChat = (chatIndex) => {
+  console.log("Selected chat", chatIndex);
+}
+const newChatBtn = () => setNewChatVisible(true)
+
   return (
     <div>
-      <p>Dashboard </p>
+      <p>{userName} Dashboard</p>
+      <ChatList
+        history={history}
+        newChatBtn={newChatBtn}
+        selectChat={selectChat}
+        chats={chats}
+        userEmail={userEmail}
+        userName={userName}
+        selectedChat={selectedChat}
+      />
     </div>
   )
 };
