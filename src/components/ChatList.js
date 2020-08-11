035142298b7d5ba83d524d61aea678fe3d801dba
Michal Borowski <unpolacoo@gmail.com>
2020-08-11 11:40:39 +0200

    Create ChatList container

---
+++ src/components/ChatList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function ChatList(props) {
+	const newChat = () => {
+		console.log('new chat clicked');
+	};
+  const selectChat = (index) => {
+    console.log('selected chat index is', index);
+  }
+	return (
+		<div>
+			<p>Hello from Chatlist!</p>
+			<button onClick={newChat}>New chat</button>
+			<ul>
+				{props.chats.map((chat, index) => {
+					return (
+            <li key={index} onClick={() => {selectChat(index)}}>
+              <p>
+                {chat.users.filter( user => user !== props.userEmail)[0]}
+              </p>
+              <p>
+                {chat.messages[chat.messages.length - 1].message.substring(0,30)}
+              </p>
+            </li>
+          )
+				})}
+			</ul>
+		</div>
+	);
+}
